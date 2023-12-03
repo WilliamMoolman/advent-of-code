1fdb5d55ae53e7f3846f2ecf5a626fa097c23a82
William Moolman <moolman.william@gmail.com>
2023-12-03 11:32:56 +0000

    refactor(2023): make day 3 idiomatic

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,68 +1,54 @@
 advent_of_code::solution!(3);
 
-fn contains_symbol(char_slice: Vec<char>) -> bool {
-    let mut contains = false;
-    for c in char_slice {
-        contains |= c != '.' && !c.is_digit(10)
-    }
-    contains
+fn contains_symbol(char_slice: &[char]) -> bool {
+    char_slice.iter().any(|&c| c != '.' && !c.is_digit(10))
 }
 
-fn symbol_surrounding(lines: &Vec<Vec<char>>, i: usize, start: usize, end: usize) -> bool {
-    let start_idx = if start == 0 { 0 } else { start - 1 };
+fn symbol_surrounding(lines: &[Vec<char>], i: usize, start: usize, end: usize) -> bool {
+    let start_idx = start.saturating_sub(1);
     let end_idx = (end + 1).min(lines[0].len());
     let mut add = false;
 
     // Check above
     if i != 0 {
-        add |= contains_symbol(lines[i - 1][start_idx..end_idx].to_vec());
+        add |= contains_symbol(&lines[i - 1][start_idx..end_idx]);
     }
     // Check mid
-    add |= contains_symbol(lines[i][start_idx..end_idx].to_vec());
+    add |= contains_symbol(&lines[i][start_idx..end_idx]);
 
     // Check after
     if i != lines.len() - 1 {
-        add |= contains_symbol(lines[i + 1][start_idx..end_idx].to_vec());
+        add |= contains_symbol(&lines[i + 1][start_idx..end_idx]);
     }
 
     add
 }
 
 fn extract_num(line: &Vec<char>, c_idx: usize) -> (u32, usize) {
-    let mut right_digits = vec![line[c_idx]];
-    for j in (c_idx + 1)..line.len() {
-        let c = line[j];
-        if c.is_digit(10) {
-            right_digits.push(c);
-        } else {
-            break;
-        }
-    }
-    let mut left_digits: Vec<char> = Vec::new();
-    for j in (0..c_idx).rev() {
-        let c = line[j];
-        if c.is_digit(10) {
-            left_digits.push(c);
-        } else {
-            break;
-        }
-    }
-    left_digits.reverse();
-    left_digits.extend(right_digits);
-    let num = String::from(left_digits.into_iter().collect::<String>())
-        .parse()
-        .unwrap();
-    (num, num.to_owned().to_string().len())
+    let right_digits = line[c_idx..]
+        .iter()
+        .take_while(|&&c| c.is_digit(10))
+        .cloned()
+        .collect::<Vec<char>>();
+
+    let left_digits: Vec<char> = line[..c_idx]
+        .iter()
+        .rev()
+        .take_while(|&&c| c.is_digit(10))
+        .cloned()
+        .collect::<Vec<char>>();
+
+    let mut digits = left_digits;
+    digits.reverse();
+    digits.extend(right_digits);
+    let num = digits.into_iter().collect::<String>().parse().unwrap();
+    (num, num.to_string().len())
 }
 
-fn extract_nums(lines: &Vec<Vec<char>>, i: usize, c_idx: usize, nums: &mut Vec<u32>) {
-    let l = if c_idx != 0 { lines[i][c_idx - 1] } else { 'x' };
+fn extract_nums(lines: &[Vec<char>], i: usize, c_idx: usize, nums: &mut Vec<u32>) {
+    let l = lines[i].get(c_idx.wrapping_sub(1)).copied().unwrap_or('x');
     let m = lines[i][c_idx];
-    let r = if c_idx != lines[0].len() - 1 {
-        lines[i][c_idx + 1]
-    } else {
-        'x'
-    };
+    let r = lines[i].get(c_idx + 1).copied().unwrap_or('x');
 
     match (l.is_digit(10), m.is_digit(10), r.is_digit(10)) {
         (_, true, _) => nums.push(extract_num(&lines[i], c_idx).0),
@@ -77,7 +63,7 @@ fn extract_nums(lines: &Vec<Vec<char>>, i: usize, c_idx: usize, nums: &mut Vec<u
     }
 }
 
-fn handle_gear(lines: &Vec<Vec<char>>, i: usize, c_idx: usize) -> Option<u32> {
+fn handle_gear(lines: &[Vec<char>], i: usize, c_idx: usize) -> Option<u32> {
     let mut nums = Vec::new();
     if i != 0 {
         extract_nums(lines, i - 1, c_idx, &mut nums)
