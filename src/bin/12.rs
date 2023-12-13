ef9866d2a25e42dd7cc3a07c9cb29ff3c4cd5d6b
William Moolman <moolman.william@gmail.com>
2023-12-13 10:44:32 +0000

    feat(2023): day 12 pt 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,5 +1,4 @@
-use memoize::memoize;
-use std::iter::repeat;
+use std::{collections::HashMap, iter::repeat};
 
 use itertools::Itertools;
 
@@ -33,267 +32,103 @@ impl Record {
         }
     }
 
-    fn schematic_as(&self, mask: usize) -> Vec<char> {
-        Record::schematic_as_static(&self.schematic, mask)
-    }
-
-    fn schematic_as_static(schematic: &Vec<char>, mask: usize) -> Vec<char> {
-        let mut n = -1;
-        schematic
-            .iter()
-            .map(|c| match c {
-                '?' => {
-                    n += 1;
-                    if mask & (1 << n) == 0 {
-                        '.'
-                    } else {
-                        '#'
-                    }
-                }
-                _ => *c,
-            })
-            .collect()
-    }
-
-    fn does_schematic_fit(&self, schematic: &Vec<char>) -> bool {
-        let hashtags: Vec<usize> = schematic
-            .iter()
-            .group_by(|&&c| c == '#')
-            .into_iter()
-            .filter_map(|(v, tags)| if v { Some(tags.count()) } else { None })
-            .collect();
-        if self.validation.len() != hashtags.len() {
-            return false;
-        }
-        for (i, &valid_size) in self.validation.iter().enumerate() {
-            if valid_size as usize != hashtags[i] {
+    fn check_pattern_suffix(&self, last: usize) -> bool {
+        for sch_idx in self.schematic.len() - last..self.schematic.len() {
+            if self.schematic[sch_idx] == '#' {
                 return false;
             }
         }
-        return true;
+        true
     }
-
-    fn how_much_does_schematic_fit(schematic: &Vec<char>, validation: &[u32]) -> Option<usize> {
-        let hashtags: Vec<usize> = schematic
-            .iter()
-            .group_by(|&&c| c == '#')
-            .into_iter()
-            .filter_map(|(v, tags)| if v { Some(tags.count()) } else { None })
-            .collect();
-        if validation.len() < hashtags.len() {
-            return None;
-        }
-        for (i, &tag_size) in hashtags.iter().enumerate() {
-            if validation[i] as usize != tag_size {
-                return None;
+    fn check_pattern_prefix(&self, start_idx: usize, buffer: usize, validation_idx: usize) -> bool {
+        for idx in 0..buffer {
+            if self.schematic[start_idx + idx] == '#' {
+                return false;
             }
         }
-
-        return Some(validation.len() - hashtags.len());
+        for idx in 0..self.validation[validation_idx] as usize {
+            if start_idx + buffer + idx >= self.schematic.len() {
+                return false;
+            }
+            if self.schematic[start_idx + buffer + idx] == '.' {
+                return false;
+            }
+        }
+        true
     }
 
-    fn schematic_as_grouped(&self, mask: usize) -> Vec<Vec<char>> {
-        let mut n = -1;
-        let mut groups: Vec<Vec<char>> = Vec::new();
-        let mut schematic_iter = self.schematic.iter();
-        loop {
-            let _ = schematic_iter.by_ref().take_while(|&&c| c == '.');
-            let group = schematic_iter
-                .by_ref()
-                .take_while(|&&c| c != '.')
-                .copied()
-                .collect_vec();
-            if group.len() == 0 {
+    fn get_max_buffer(&self) -> usize {
+        self.schematic.len() - self.validation.iter().sum::<u32>() as usize
+    }
+
+    fn combinations(
+        &self,
+        start_idx: usize,
+        max_buffer: usize,
+        validation_idx: usize,
+        memoize: &mut HashMap<(usize, usize, usize), u64>,
+        depth: usize,
+    ) -> u64 {
+        if validation_idx == self.validation.len() {
+            if self.check_pattern_suffix(max_buffer) {
+                return 1;
+            } else {
+                return 0;
+            };
+        }
+        let min_buffer = if validation_idx == 0 { 0 } else { 1 };
+        let mut sum = 0;
+        for buffsize in min_buffer..=min_buffer + max_buffer {
+            if !self.check_pattern_prefix(start_idx, buffsize, validation_idx) {
+                continue;
+            }
+            let next_start = start_idx + buffsize + self.validation[validation_idx] as usize;
+            if max_buffer < buffsize {
                 break;
             }
-            groups.push(group);
-        }
-        groups
-    }
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let records: Vec<Record> = input.lines().map(Record::from_line).collect();
-    let mut combinations = 0;
-    for record in records.iter() {
-        // println!("{record:?}");
-        let num_question = record.schematic.iter().filter(|&&c| c == '?').count();
-        // println!("?x{num_question}");
-        for i in 0..2usize.pow(num_question as u32) {
-            let new_schem = record.schematic_as(i);
-            let works = record.does_schematic_fit(&new_schem);
-
-            if works {
-                combinations += 1;
+            let next_buffer = max_buffer - buffsize;
+            let next_validation_idx = validation_idx + 1;
+            if let Some(combinations) = memoize.get(&(next_start, next_buffer, next_validation_idx))
+            {
+                sum += combinations;
+            } else {
+                let combinations = self.combinations(
+                    next_start,
+                    next_buffer,
+                    next_validation_idx,
+                    memoize,
+                    depth + 1,
+                );
+                memoize.insert((next_start, next_buffer, next_validation_idx), combinations);
+                sum += combinations;
             }
         }
+        sum
     }
-    Some(combinations)
 }
 
-fn get_counts_for_group(schematic: &Vec<char>, validation_remaining: &[u32]) -> Vec<u64> {
-    // Need to remove this part!!!! grows to like >2^40
-    // let num_question = current_group.iter().filter(|&&c| c == '?').count();
-    // for i in 0..2usize.pow(num_question as u32) {
-    //     let new_schem = Record::schematic_as_static(current_group, i);
-    //     if let Some(num) = Record::how_much_does_schematic_fit(&new_schem, validation_remaining) {
-    //         counts[num] += 1;
-    //     }
-    // }
-    //
-    println!("GROUP: {}", schematic.iter().collect::<String>());
-    let min_version: Vec<char> = schematic
-        .iter()
-        .map(|&c| match c {
-            '?' => '.',
-            _ => c,
-        })
-        .collect();
-    let max_version: Vec<char> = schematic
-        .iter()
-        .map(|&c| match c {
-            '?' => '#',
-            _ => c,
-        })
-        .collect();
-
-    let mut min: Vec<usize> = Vec::new();
-    let mut max: Vec<usize> = Vec::new();
-
-    let mut idx = 0;
-    while idx < min_version.len() {
-        if min_version[idx] == '.' {
-            min.push(0);
-            idx += 1;
-            continue;
-        }
-        let mut j = 0;
-        while idx + j < min_version.len() && min_version[idx + j] == '#' {
-            j += 1;
-        }
-        for _ in 0..j {
-            min.push(j);
-        }
-        idx += j;
+pub fn part_one(input: &str) -> Option<u64> {
+    let records: Vec<Record> = input.lines().map(Record::from_line).collect();
+    let mut sum = 0;
+    for record in records {
+        let max_buffer = record.get_max_buffer();
+        let mut memoize: HashMap<(usize, usize, usize), u64> = HashMap::new();
+        let comb = record.combinations(0, max_buffer, 0, &mut memoize, 0);
+        sum += comb;
     }
-    let mut idx = 0;
-    while idx < max_version.len() {
-        if max_version[idx] == '.' {
-            max.push(0);
-            idx += 1;
-            continue;
-        }
-        let mut j = 0;
-        while idx + j < max_version.len() && max_version[idx + j] == '#' {
-            j += 1;
-        }
-        for _ in 0..j {
-            max.push(j);
-        }
-        idx += j;
-    }
-
-    println!(
-        "MIN:   {}",
-        min.iter().map(|d| d.to_string()).collect::<String>()
-    );
-    println!(
-        "MAX:   {}",
-        max.iter().map(|d| d.to_string()).collect::<String>()
-    );
-    // Call this the flattening
-    let mut counts = vec![0; validation_remaining.len() + 1];
-    // let s_idx = 0;
-    // let v_idx = 0;
-
-    // Need to remove this part!!!! grows to like >2^40
-
-    let num_question = schematic.iter().filter(|&&c| c == '?').count();
-    for i in 0..2usize.pow(num_question as u32) {
-        let new_schem = Record::schematic_as_static(schematic, i);
-        if let Some(num) = Record::how_much_does_schematic_fit(&new_schem, validation_remaining) {
-            counts[num] += 1;
-        }
-    }
-    counts
-}
-
-fn get_combinations(schematic_groups: &[Vec<char>], validation_remaining: &[u32]) -> u64 {
-    if schematic_groups.len() == 0 {
-        return 0;
-    }
-    let current_group = &schematic_groups[0];
-    let counts = get_counts_for_group(current_group, validation_remaining);
-
-    //
-    let mut total = counts[0];
-    for i in 1..counts.len() {
-        if counts[i] == 0 {
-            continue;
-        }
-        total += counts[i] * get_combinations(&schematic_groups[1..], &validation_remaining[i..]);
-    }
-    total
-}
-
-fn rec_get_combinations(schematic: &[char], validation: &[u32]) -> u64 {
-    if schematic[0] == '?' {
-        return rec_get_combinations(schematic, validation);
-    }
-    let mut n = -1;
-    let mut groups: Vec<Vec<char>> = Vec::new();
-    let mut schematic_iter = self.schematic.iter();
-    loop {
-        let _ = schematic_iter.by_ref().take_while(|&&c| c == '.');
-        let group = schematic_iter
-            .by_ref()
-            .take_while(|&&c| c != '.')
-            .copied()
-            .collect_vec();
-        if group.len() == 0 {
-            break;
-        }
-        groups.push(group);
-    }
-    groups
+    Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let records: Vec<Record> = input.lines().map(Record::from_line_5x).collect();
-    let mut combinations = 0;
-    for record in records.iter() {
-        println!("{record:?} ==>");
-        // let num_question = record.schematic.iter().filter(|&&c| c == '?').count();
-        // let groups: Vec<Vec<char>> = record.schematic_as_grouped(0);
-        // let total = get_combinations(&groups, &record.validation[..]);
-        // println!("<== Total: {total}");
-        // combinations += total;
-        let num_question = record.schematic.iter().filter(|&&c| c == '?').count();
-
-        for i in 0..2usize.pow(num_question as u32) {
-            let new_schem = record.schematic_as(i);
-            let works = record.does_schematic_fit(&new_schem);
-
-            if works {
-                combinations += 1;
-            }
-        }
-
-        // return None;
-        // println!("?x{num_question}");
-        // println!("Iterating over {:e}", 2usize.pow(num_question as u32));
-        // for i in 0..2usize.pow(num_question as u32) {
-        //     let new_schem = record.schematic_as(i);
-        //     let works = record.does_schematic_fit(&new_schem);
-
-        //     if works {
-        //         combinations += 1;
-        //     }
-        // }
+    let mut sum = 0;
+    for record in records {
+        let max_buffer = record.get_max_buffer();
+        let mut memoize: HashMap<(usize, usize, usize), u64> = HashMap::new();
+        sum += record.combinations(0, max_buffer, 0, &mut memoize, 0);
     }
-    Some(combinations)
+    Some(sum)
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
